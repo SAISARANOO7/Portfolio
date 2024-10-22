b18e245fecb471eb6861cc2e6d745bc7212fac91
Sai Saran <sai.saran@example.com>
2024-10-22 19:46:33 +0530

    Added Portfolio

---
+++ index.js
@@ -0,0 +1,21 @@
+let header = document.querySelector("header");
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("shadow", window.scrollY > 0);
+});
+
+menu.onclick = () => {
+  navbar.classList.toggle("active");
+};
+window.onscroll = () => {
+  navbar.classList.remove("active");
+};
+
+document.getElementById('resume-button-1').addEventListener("click", () => {
+  window.open("https://drive.google.com/file/d/1UAj5f-fHiInGHRnJJjCvNHGGwzpm3RMo/view?usp=share_link", "_blank");
+});
+
+
+  
